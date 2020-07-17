b9c341251e2b0f0684a96f20269893e02410770a
Andrea Brambilla <abramb@equinor.com>
2020-07-17 10:32:46 +0200

    Setting up Babel and Jest
    - Renamed the `static` folder to `src`. 
    - Set up Babel. Running `npm run build` now transpiles files in `src` and the output is in `static`
    - Set up Jest. Running `npm test` now runs all the tests in the test folder
    
    Babel has been set up following the _Overview_ section at https://babeljs.io/docs/en/usage 
    
    The Jest config was generated by running `./node_modules/jest/bin/jest.js --init`
    
    This configuration was tested on node 12.18.2 and npm 6.14.6

---
+++ tests/modules/square.test.js
@@ -0,0 +1,7 @@
+import * as square from '../../static/modules/square.js'
+
+// const square = require( '../../static/modules/square.js')
+
+test('name is square', () => {
+    expect(square.name).toBe('square')
+})

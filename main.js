e686f56246d28eb2fbffdb829264f512542d361d
andreabrambilla <andrea.bram@gmail.com>
2020-07-17 08:49:59 +0000

    Deploying to gh-pages from  @ 96c4818176163d5cab3e7673a4295bab60dec455 🚀

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-import { create, createReportList } from './modules/canvas.js';
-import { name, draw, reportArea, reportPerimeter } from './modules/square.js';
-import randomSquare from './modules/square.js';
+"use strict";
 
-let myCanvas = create('myCanvas', document.body, 480, 320);
-let reportList = createReportList(myCanvas.id);
+var _canvas = require("./modules/canvas.js");
 
-let square1 = draw(myCanvas.ctx, 50, 50, 100, 'blue');
-reportArea(square1.length, reportList);
-reportPerimeter(square1.length, reportList);
+var _square = _interopRequireWildcard(require("./modules/square.js"));
 
-// Use the default
-let square2 = randomSquare(myCanvas.ctx);
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+let myCanvas = (0, _canvas.create)('myCanvas', document.body, 480, 320);
+let reportList = (0, _canvas.createReportList)(myCanvas.id);
+let square1 = (0, _square.draw)(myCanvas.ctx, 50, 50, 100, 'blue');
+(0, _square.reportArea)(square1.length, reportList);
+(0, _square.reportPerimeter)(square1.length, reportList); // Use the default
+
+let square2 = (0, _square.default)(myCanvas.ctx);
--- modules/square.js
+++ modules/square.js
@@ -1,9 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.draw = draw;
+exports.reportArea = reportArea;
+exports.reportPerimeter = reportPerimeter;
+exports.default = exports.name = void 0;
 const name = 'square';
+exports.name = name;
 
 function draw(ctx, length, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, length, length);
-
   return {
     length: length,
     x: x,
@@ -13,22 +22,20 @@ function draw(ctx, length, x, y, color) {
 }
 
 function random(min, max) {
-   let num = Math.floor(Math.random() * (max - min)) + min;
-   return num;
+  let num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
 }
 
 function reportArea(length, listId) {
   let listItem = document.createElement('li');
-  listItem.textContent = `${name} area is ${length * length}px squared.`
-
+  listItem.textContent = `${name} area is ${length * length}px squared.`;
   let list = document.getElementById(listId);
   list.appendChild(listItem);
 }
 
 function reportPerimeter(length, listId) {
   let listItem = document.createElement('li');
-  listItem.textContent = `${name} perimeter is ${length * 4}px.`
-
+  listItem.textContent = `${name} perimeter is ${length * 4}px.`;
   let list = document.getElementById(listId);
   list.appendChild(listItem);
 }
@@ -37,14 +44,12 @@ function randomSquare(ctx) {
   let color1 = random(0, 255);
   let color2 = random(0, 255);
   let color3 = random(0, 255);
-  let color = `rgb(${color1},${color2},${color3})`
+  let color = `rgb(${color1},${color2},${color3})`;
   ctx.fillStyle = color;
-
   let x = random(0, 480);
   let y = random(0, 320);
   let length = random(10, 100);
   ctx.fillRect(x, y, length, length);
-
   return {
     length: length,
     x: x,
@@ -53,5 +58,5 @@ function randomSquare(ctx) {
   };
 }
 
-export { name, draw, reportArea, reportPerimeter };
-export default randomSquare;
+var _default = randomSquare;
+exports.default = _default;
